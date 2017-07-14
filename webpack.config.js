4c00bfd220b555b17e9d9208137357a16d9b35d5
mateuszrosa <mateusz.rosa@gmail.com>
2017-07-14 15:29:48 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: ["whatwg-fetch", "./js/app.jsx"],
+    output: {
+        path: path.resolve(__dirname, "./js"),
+        publicPath: '/js',
+        filename: "out.js"
+    },
+    devServer: {
+        inline: true,
+        hot: true,
+        contentBase: './',
+        port: 3003
+    },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                // include: path.join(__dirname, 'images'),
+                loader: 'url-loader?limit=1000000'
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=public/fonts/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin( './../css/main.css' )
+    ]
+}
