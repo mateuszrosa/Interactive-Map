228c297f86752ed32d4c8aabd3f6af3e76973f2e
mateuszrosa <mateusz.rosa@gmail.com>
2019-04-19 17:57:01 +0200

    webpack config edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,20 @@
-const path = require("path");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-
 module.exports = {
-    entry: ["whatwg-fetch", "./js/app.jsx"],
+    entry: "./js/script1.js",
     output: {
-        path: path.resolve(__dirname, "./js"),
-        publicPath: '/js',
-        filename: "out.js"
-    },
-    devServer: {
-        inline: true,
-        hot: true,
-        contentBase: './',
-        port: 3003
+        filename: "./js/out.js"
     },
     watch: true,
     module: {
-        loaders: [
-            {
-                test: /\.jsx$/,
+        loaders: [{
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
-                query: { presets: ['es2015', 'stage-2', 'react'] }
+                query: { presets: ['es2015'] }
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                // include: path.join(__dirname, 'images'),
-                loader: 'url-loader?limit=1000000'
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader?name=public/fonts/[name].[ext]'
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin( './../css/main.css' )
-    ]
+    }
 }
