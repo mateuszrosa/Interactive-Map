fa8cb6ae5a0179d990d943bd531eee4497ead6a0
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-07 12:48:25 +0200

    contructor variables

--- js/app1.js
+++ js/app1.js
@@ -3,7 +3,19 @@ class WorldMap {
         this.map = document.querySelectorAll('.land');
         this.map.forEach(map => {
             map.addEventListener('click', this.startApp.bind(this));
-        })
+        });
+        this.input = document.querySelector('input');
+        this.form = document.querySelector('form');
+        this.name = document.querySelector('h3:nth-of-type(2)');
+        this.region = document.querySelector("h3:nth-of-type(3)");
+        this.subregion = document.querySelector("h3:nth-of-type(4)");
+        this.nativeName = document.querySelector("h3:nth-of-type(5)");
+        this.capital = document.querySelector("h3:nth-of-type(6)");
+        this.language = document.querySelector("h3:nth-of-type(7");
+        this.currency = document.querySelector("h3:nth-of-type(8)");
+        this.population = document.querySelector("h3:nth-of-type(9)");
+        this.img = document.querySelector(".info img");
+        this.infos = document.querySelectorAll('.infos');
     }
     getTitle(e) {
         return e.target
@@ -25,4 +37,4 @@ class WorldMap {
 
 setTimeout(() => {
     const world = new WorldMap;
-}, 300);
+}, 500);
