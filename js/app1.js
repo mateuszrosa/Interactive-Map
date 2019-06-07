df0955d53bfc69f222cf4925ee1fd73e6afef26d
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-07 13:34:02 +0200

    filling infos about countries

--- js/app1.js
+++ js/app1.js
@@ -17,8 +17,11 @@ class WorldMap {
         this.img = document.querySelector(".info img");
         this.infos = document.querySelectorAll('.infos');
     }
-    getTitle(e) {
+    getLand(e) {
         return e.target
+    }
+    getTitle(e) {
+        return e.target.getAttribute('title');
     }
     fillCountry(country) {
         country.style.fill = "gray"
@@ -28,10 +31,58 @@ class WorldMap {
             map.style.fill = "#ac9d93"
         });
     }
+    checkValue(value) {
+        if (value === "South Korea") {
+            value = "Korea (Republic of)";
+        } else if (value === "North Korea") {
+            value = "Korea (Democratic People's Republic of)";
+        } else if (value === "Republic of Congo") {
+            value = "Congo";
+        } else if (value === "Democratic Republic of Congo") {
+            value = "Congo (Democratic Republic of the)"
+        }
+        return value;
+    }
+    fillInfo(value) {
+        console.log(value);
+        fetch(`https://restcountries.eu/rest/v2/name/` + value)
+            .then(resp => {
+                return resp.json()
+            })
+            .then(data => {
+                let country = data[0];
+                console.log(country);
+                if (value === "India") {
+                    country = data[1];
+                }
+                this.name.innerHTML = `Name: <span class="inf">${country.name}</span>`
+                this.region.innerHTML = `Region: <span class="inf">${country.region}</span>`
+                this.subregion.innerHTML = `Subegion: <span class="inf">${country.subregion}</span>`
+                this.nativeName.innerHTML = `Native name: <span class="inf">${country.nativeName}</span>`
+                this.capital.innerHTML = `Capital: <span class="inf">${country.capital}</span>`
+                this.language.innerHTML = `Language: <span class="inf">${country.languages[0].name}</span>`
+                this.currency.innerHTML = `Currency: <span class="inf">${country.currencies[0].code}</span>`
+                this.population.innerHTML = `Population: <span class="inf">${country.population.toLocaleString()}</span>`
+                this.img.setAttribute("src", country.flag);
+                this.img.style.boxShadow = '0 0 3px 3px gray';
+                if (document.querySelector("body > section.info > h3:nth-child(3) > span").textContent === "Korea (Republic of)") {
+                    document.querySelector("body > section.info > h3:nth-child(3) > span").textContent = "Republic of Korea";
+                } else if (document.querySelector("body > section.info > h3:nth-child(3) > span").textContent === `Korea (Democratic People's Republic of)`) {
+                    document.querySelector("body > section.info > h3:nth-child(3) > span").textContent = "Democratic People's Republic of Korea";
+                } else if (document.querySelector("body > section.info > h3:nth-child(3) > span").textContent === `Congo (Democratic Republic of the)`) {
+                    document.querySelector("body > section.info > h3:nth-child(3) > span").textContent = "Democratic Republic of the Congo";
+                }
+
+            })
+
+    }
     startApp(e) {
-        const item = this.getTitle(e);
+        const item = this.getLand(e);
+        const title = this.getTitle(e);
+        const value = this.checkValue(title)
         this.fillCountries();
         this.fillCountry(item);
+        this.fillInfo(value);
     }
 }
 
